e22053c2f4d94023e550fc5e67d861737ebf5e81
Ana Franco <anamspe.fr@gmail.com>
2024-04-01 18:20:32 -0700

    Add Projects and Skill cards to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import HeroCard from "./components/HeroCard";
 import NavBar from "./components/NavBar";
+import Projects from "./components/ProjectsCard";
+import Skills from "./components/SkillsCard";
 
 const App = () => {
   return (
-    <div className=" bg-eerie_black-100 h-screen">
+    <div className=" bg-eerie_black-200 min-h-screen">
       <NavBar />
       <HeroCard />
       <hr class="mx-auto w-1/2" />
+      <Projects />
+      <hr class="mx-auto w-1/2" />
+      <Skills />
     </div>
   );
 };
