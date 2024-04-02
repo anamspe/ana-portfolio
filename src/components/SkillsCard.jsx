3d0174ba45ff606771b38638aa929393849a9931
Ana Franco <anamspe.fr@gmail.com>
2024-04-01 18:19:42 -0700

    Create Skills card

---
+++ src/components/SkillsCard.jsx
@@ -0,0 +1,9 @@
+const Skills = () => {
+  return (
+    <div className="flex gap-10 my-12 p-10 border">
+      <h2 className="text-2xl text-cherry">Skills</h2>
+    </div>
+  );
+};
+
+export default Skills;
