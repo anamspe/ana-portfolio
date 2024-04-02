2f8359ba8bee73643517a9edfad7136fdf7bb48c
Ana Franco <anamspe.fr@gmail.com>
2024-04-01 18:19:27 -0700

    Create Projects card

---
+++ src/components/ProjectsCard.jsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+  return (
+    <div className="flex gap-10 my-12 p-10 border">
+      <h2 className="text-2xl text-cherry">Projects</h2>
+    </div>
+  );
+};
+
+export default Projects;
