2f7795e8996d199b5e861b319b9b4b4deba15e43
Ana Franco <anamspe.fr@gmail.com>
2024-04-04 14:37:46 -0700

    Create projects object

---
+++ src/assets/projects.js
@@ -0,0 +1,41 @@
+const projects = {
+  1: {
+    name: "Acme Rockets",
+    description:
+      "The Acme Rockets landing page is an e-commerce platform for a ficticious rocket store, made entirely with HTML, Tailwind CSS and Vanilla JavaScript. It counts with a responsive design and selective dark mode, that will be remembered for next visits.",
+    repoLink: "https://github.com/anamspe/acme-rockets-tailwind",
+    website: "https://acmerockets-erim.onrender.com/",
+    stack: ["html", "tailwind css", "javascript"],
+  },
+  2: {
+    name: "DateSync",
+    description:
+      "DateSync is the place to plan and create the best dates for you to share with friends and loved ones. Full-Stack application created with a PERN Stack that makes use of Google Locations API to serve users with suggestions for their next outing with family and friends. ",
+    repoLink: "https://github.com/anamspe/datesync",
+    stack: [
+      "react",
+      "node",
+      "express",
+      "postgresql",
+      "material ui",
+      "google api",
+      "auth0",
+      "sass",
+    ],
+  },
+  3: {
+    name: "HaggleHub",
+    description: "HaggleHub is a B2C e-commerce platform focused in selling Funko Pop collectibles. Full-Stack application that includes admin and customer interfaces depending on credentials when user logs in to the app.",
+    repoLink: "https://github.com/anamspe/Hagglehub",
+    stack: ["javascript", "ejs", "node", "postgresql", "sass"]
+  },
+  4: {
+    name: "WeatherApp",
+    description: "This weather app created with the OpenWeather API allows user to search their city and check weather pertaining information, such as: weather temperature, wind speed, humidity, heat index, and sunrise and sunset times",
+    repoLink: "https://github.com/anamspe/react-temp-app",
+    website: "https://elegant-macaron-6f91d4.netlify.app/",
+    stack: ["react", "css", "openweather api"]
+  }
+};
+
+export default projects;
