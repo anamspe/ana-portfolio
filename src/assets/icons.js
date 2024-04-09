2f31731a6af6371b55db6006abaa4b2c0c350a00
Ana Franco <anamspe.fr@gmail.com>
2024-04-08 20:31:36 -0700

    Prettier fixes

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,36 +1,48 @@
 const icons = {
   js: {
     src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/javascript/javascript-original.svg",
-    alt: "JavaScript"
+    alt: "JavaScript",
   },
   react: {
     src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/react/react-original.svg",
-    alt: "React"
+    alt: "React",
   },
   html: {
     src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/html5/html5-original.svg",
-    alt: "HTML"
+    alt: "HTML",
   },
   css: {
     src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/css3/css3-original.svg",
-    alt: "CSS"
+    alt: "CSS",
   },
   node: {
     src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/nodejs/nodejs-original.svg",
-    alt: "Node.js"
+    alt: "Node.js",
   },
   tailwindcss: {
     src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/tailwindcss/tailwindcss-original.svg",
-    alt: "Tailwind CSS"
+    alt: "Tailwind CSS",
   },
   express: {
-    src:"https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/express/express-original-wordmark.svg",
-    alt:"Express"
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/express/express-original.svg",
+    alt: "Express",
   },
   ejs: {
     src: "../../public/img/ejs_icon_132422.png",
-    alt: "ejs - Embedded JavaScript"
-  }
+    alt: "ejs - Embedded JavaScript",
+  },
+  postgresql: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/postgresql/postgresql-original.svg",
+    alt: "PostgreSQL",
+  },
+  materialui: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/materialui/materialui-original.svg",
+    alt: "Material UI",
+  },
+  sass: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/sass/sass-original.svg",
+    alt: "Sass",
+  },
 };
 
 export default icons;
--- src/assets/projects.js
+++ src/assets/projects.js
@@ -10,43 +10,37 @@ const projects = {
   },
   2: {
     name: "DateSync",
-    img: "../../public/img/datesync-crop.png",    
+    img: "../../public/img/datesync-crop.png",
     description:
       "DateSync is the place to plan and create the best dates for you to share with friends and loved ones. Full-Stack application created with a PERN Stack that makes use of Google Locations API to serve users with suggestions for their next outing with family and friends. ",
     repoLink: "https://github.com/anamspe/datesync",
-    stack: [
-      "react",
-      "node",
-      "express",
-      "postgresql",
-      "material ui",
-      "google api",
-      "auth0",
-      "sass",
-    ],
+    stack: ["react", "node", "express", "postgresql", "materialui", "sass"],
   },
   3: {
     name: "HaggleHub",
     img: "../../public/img/hagglehub-crop.png",
-    description: "HaggleHub is a B2C e-commerce platform focused in selling Funko Pop collectibles. Full-Stack application that includes admin and customer interfaces depending on credentials when user logs in to the app.",
+    description:
+      "HaggleHub is a B2C e-commerce platform focused in selling Funko Pop collectibles. Full-Stack application that includes admin and customer interfaces depending on credentials when user logs in to the app.",
     repoLink: "https://github.com/anamspe/Hagglehub",
-    stack: ["js", "ejs", "node", "postgresql", "sass"]
+    stack: ["js", "ejs", "node", "postgresql", "sass"],
   },
   4: {
     name: "WeatherApp",
     img: "../../public/img/weather-app-crop.png",
-    description: "This weather app created with the OpenWeather API allows user to search their city and check weather pertaining information, such as: weather temperature, wind speed, humidity, heat index, and sunrise and sunset times",
+    description:
+      "This weather app created with the OpenWeather API allows user to search their city and check weather pertaining information, such as: weather temperature, wind speed, humidity, heat index, and sunrise and sunset times",
     repoLink: "https://github.com/anamspe/react-temp-app",
     website: "https://elegant-macaron-6f91d4.netlify.app/",
-    stack: ["react", "css", "openweather api"]
+    stack: ["react", "css"],
   },
   5: {
     name: "TinyApp",
     img: "../../public/img/tinyapp-crop.png",
-    description: "TinyApp is a URL manager that allows users to shorten and edit links, accessible through a user-friendly interface. Full-Stack aplication, built with Node.js and Express, it is exclusive to registered users, offering secure and efficient URL handling.",
-    repoLink: "https://github.com/anamspe/tinyapp",    
-    stack: ["html", "ejs", "css", "js", "node", "express"]
-  }
+    description:
+      "TinyApp is a URL manager that allows users to shorten and edit links, accessible through a user-friendly interface. Full-Stack aplication, built with Node.js and Express, it is exclusive to registered users, offering secure and efficient URL handling.",
+    repoLink: "https://github.com/anamspe/tinyapp",
+    stack: ["js", "ejs", "html", "css", "node", "express"],
+  },
 };
 
 export default projects;
