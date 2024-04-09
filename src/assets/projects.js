603abfcef87415c96708306ab75817bb0fe719e1
Ana Franco <anamspe.fr@gmail.com>
2024-04-08 18:13:05 -0700

    Add new screenshots

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,7 +1,7 @@
 const projects = {
   1: {
     name: "Acme Rockets",
-    img: "https://github.com/anamspe/acme-rockets-tailwind/blob/main/build/public/acme-rockets.png?raw=true",
+    img: "../../public/img/acme-rockets.png",
     description:
       "The Acme Rockets landing page is an e-commerce platform for a ficticious rocket store, made entirely with HTML, Tailwind CSS and Vanilla JavaScript. It counts with a responsive design and selective dark mode, that will be remembered for next visits.",
     repoLink: "https://github.com/anamspe/acme-rockets-tailwind",
@@ -10,7 +10,7 @@ const projects = {
   },
   2: {
     name: "DateSync",
-    img: "https://github.com/anamspe/datesync/blob/main/src/client/assets/screenshots/landing-page-desktop.png?raw=true",
+    img: "../../public/img/datesync-crop.png",    
     description:
       "DateSync is the place to plan and create the best dates for you to share with friends and loved ones. Full-Stack application created with a PERN Stack that makes use of Google Locations API to serve users with suggestions for their next outing with family and friends. ",
     repoLink: "https://github.com/anamspe/datesync",
@@ -27,18 +27,25 @@ const projects = {
   },
   3: {
     name: "HaggleHub",
-    img: "https://github.com/DavidGir/HaggleHub/blob/main/ScreenShots/Customer%20Product-page.png?raw=true",
+    img: "../../public/img/hagglehub-crop.png",
     description: "HaggleHub is a B2C e-commerce platform focused in selling Funko Pop collectibles. Full-Stack application that includes admin and customer interfaces depending on credentials when user logs in to the app.",
     repoLink: "https://github.com/anamspe/Hagglehub",
     stack: ["javascript", "ejs", "node", "postgresql", "sass"]
   },
   4: {
     name: "WeatherApp",
-    img: "https://github.com/anamspe/react-temp-app/blob/master/public/images/weather-app.png?raw=true",
+    img: "../../public/img/weather-app-crop.png",
     description: "This weather app created with the OpenWeather API allows user to search their city and check weather pertaining information, such as: weather temperature, wind speed, humidity, heat index, and sunrise and sunset times",
     repoLink: "https://github.com/anamspe/react-temp-app",
     website: "https://elegant-macaron-6f91d4.netlify.app/",
     stack: ["react", "css", "openweather api"]
+  },
+  5: {
+    name: "TinyApp",
+    img: "../../public/img/tinyapp-crop.png",
+    description: "TinyApp is a URL manager that allows users to shorten and edit links, accessible through a user-friendly interface. Full-Stack aplication, built with Node.js and Express, it is exclusive to registered users, offering secure and efficient URL handling.",
+    repoLink: "https://github.com/anamspe/tinyapp",    
+    stack: ["html", "ejs", "css", "javascript", "node", "express"]
   }
 };
 
