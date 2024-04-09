a52448cc924811a6919bb6fa11dd7d6582feb818
Ana Franco <anamspe.fr@gmail.com>
2024-04-08 19:40:09 -0700

    Add stack icons src and alt

---
+++ src/assets/icons.js
@@ -0,0 +1,36 @@
+const icons = {
+  js: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/javascript/javascript-original.svg",
+    alt: "JavaScript"
+  },
+  react: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/react/react-original.svg",
+    alt: "React"
+  },
+  html: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/html5/html5-original.svg",
+    alt: "HTML"
+  },
+  css: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/css3/css3-original.svg",
+    alt: "CSS"
+  },
+  node: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/nodejs/nodejs-original.svg",
+    alt: "Node.js"
+  },
+  tailwindcss: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/tailwindcss/tailwindcss-original.svg",
+    alt: "Tailwind CSS"
+  },
+  express: {
+    src:"https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/express/express-original-wordmark.svg",
+    alt:"Express"
+  },
+  ejs: {
+    src: "../../public/img/ejs_icon_132422.png",
+    alt: "ejs - Embedded JavaScript"
+  }
+};
+
+export default icons;
