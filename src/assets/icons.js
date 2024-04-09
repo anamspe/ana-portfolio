d34b90933bf23fef98275746a05d27812670316b
Ana Franco <anamspe.fr@gmail.com>
2024-04-09 16:51:00 -0700

    Add new icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -43,6 +43,62 @@ const icons = {
     src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/sass/sass-original.svg",
     alt: "Sass",
   },
+  ruby: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/ruby/ruby-original.svg",
+    alt: "Ruby",
+  },
+  sql: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/ruby/ruby-original.svg",
+    alt: "SQL",
+  },
+  redux: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/redux/redux-original.svg",
+    alt: "Redux",
+  },
+  rails: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/rails/rails-original-wordmark.svg",
+    alt: "Rails",
+  },
+  jquery: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/jquery/jquery-original-wordmark.svg",
+    alt: "jQuery",
+  },
+  git: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/git/git-original.svg",
+    alt: "Git",
+  },
+  mocha: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/mocha/mocha-original.svg",
+    alt: "Mocha",
+  },
+  jest: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/jest/jest-plain.svg",
+    alt: "Jest",
+  },
+  chai: {
+    src: "../../public/img/chai-icon.png",
+    alt: "Chai",
+  },
+  cypress: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/cypressio/cypressio-original.svg",
+    alt: "Cypress",
+  },
+  photoshop: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/photoshop/photoshop-original.svg",
+    alt: "Adobe Photoshop",
+  },
+  illustrator: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/illustrator/illustrator-line.svg",
+    alt: "Adobe Illustrator",
+  },
+  premiere: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/premierepro/premierepro-original.svg",
+    alt: "Adobe Premiere",
+  },
+  aftereffects: {
+    src: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/aftereffects/aftereffects-original.svg",
+    alt: "Adobe After Effects",
+  },
 };
 
 export default icons;
