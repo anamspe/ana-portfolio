7279fe24b9912df3f8069019d77420e0db450e50
Ana Franco <anamspe.fr@gmail.com>
2024-04-09 16:51:32 -0700

    Add content to SkillsCards

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,7 +1,72 @@
-const Skills = () => {
+import icons from "../assets/icons";
+
+const SkillCategory = ({ title, skills }) => {
   return (
-    <div className="flex gap-10 my-12 p-10 border">
-      <h2 className="text-2xl text-cherry">Skills</h2>
+    <div>
+      <h2>{title}</h2>
+      <ul className="">
+        {skills.map((skill, index) => (
+          <li key={index} className="display: inline-block">
+            <img
+              className="size-12 bg-cadet bg-opacity-50 mr-2 p-1 rounded"
+              src={skill.src}
+              alt={skill.alt}
+              title={skill.alt}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Skills = () => {
+  const programmingLanguages = [
+    icons["js"],
+    icons["ruby"],
+    icons["html"],
+    icons["css"]
+  ];
+
+  const frameworksAndLibraries = [
+    icons["react"],
+    icons["node"],
+    icons["redux"],
+    icons["tailwindcss"],
+    icons["sass"],
+    icons["express"],
+    icons["ejs"],
+    icons["jquery"],
+    icons["rails"]
+  ];
+
+  const systemsAndDatabases = [
+    icons["postgresql"],
+    icons["git"]
+  ];
+
+  const testingFrameworks = [
+    icons["mocha"],
+    icons["chai"],
+    icons["jest"],
+    icons["cypress"]
+  ];
+
+  const graphicDesign = [
+    icons["illustrator"],
+    icons["photoshop"],
+    icons["premiere"],
+    icons["aftereffects"]
+  ];
+
+  return (
+    <div className="flex flex-col max-w-5xl mt-12 mx-auto p-10 border">
+      <h2 className="text-4xl font-arima font-bold text-cherry p-0">Skills</h2>
+      <SkillCategory title="Programming Languages" skills={programmingLanguages} />
+      <SkillCategory title="Frameworks & Libraries" skills={frameworksAndLibraries} />
+      <SkillCategory title="Systems & Databases" skills={systemsAndDatabases} />
+      <SkillCategory title="Testing Frameworks" skills={testingFrameworks} />
+      <SkillCategory title="Graphic Design" skills={graphicDesign} />
     </div>
   );
 };
