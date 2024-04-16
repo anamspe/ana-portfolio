19788e3144bf86ba95816ec29d15742805539cd8
Ana Franco <anamspe.fr@gmail.com>
2024-04-16 12:12:28 -0700

    Update Skill's section UI

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -3,21 +3,18 @@ import { Tooltip } from "react-tooltip";
 
 const SkillCategory = ({ title, skills }) => {
   return (
-    <div>
-      <h2 className="ms-4 my-4 font-bold underline underline-offset-2">
-        {title}
-      </h2>
+    <div className="max-w-60 pl-4 mx-auto">
+      <h2 className="ms-4 my-4 font-bold min-w-60">{title}</h2>
       <ul className="ms-4">
         {skills.map((skill, index) => (
           <li
             key={index}
             className="inline-block transition-all duration-500 ease-in-out hover:opacity-70"
             data-tooltip-id="skill-tt"
-            data-tooltip-content={skill.alt}            
-            data-tooltip-delay-show={300}
-            title={skill.alt}
+            data-tooltip-content={skill.alt}
+            data-tooltip-delay-show={300}           
           >
-            <Tooltip id="skill-tt" place="bottom-start"/>
+            <Tooltip id="skill-tt" place="bottom-start" />
             <img
               className="size-12 bg-cadet bg-opacity-50 mr-2 p-1 rounded "
               src={skill.src}
@@ -67,21 +64,17 @@ const Skills = () => {
   ];
 
   return (
-    <div className="flex flex-col max-w-5xl mt-2 mx-auto p-10" id="skills">
-      <h2 className="text-4xl mt-12 font-arima font-bold text-cherry p-0 mb-4">
+    <div className="max-w-7xl mx-auto px-10">
+      <h2 className="text-4xl mt-14 p-0 pl-32 mb-4 font-arima font-bold text-cherry">
         Skills
       </h2>
-      <SkillCategory
-        title="Programming Languages"
-        skills={programmingLanguages}
-      />
-      <SkillCategory
-        title="Frameworks & Libraries"
-        skills={frameworksAndLibraries}
-      />
-      <SkillCategory title="Systems & Databases" skills={systemsAndDatabases} />
-      <SkillCategory title="Testing Frameworks" skills={testingFrameworks} />
-      <SkillCategory title="Graphic Design" skills={graphicDesign} />
+      <div className=" flex py-10 my-10 bg-tyrian bg-opacity-50 drop-shadow-sm shadow-paynes sha rounded-3xl" id="skills">
+        <SkillCategory title="Programming Languages" skills={programmingLanguages} />
+        <SkillCategory title="Frameworks & Libraries" skills={frameworksAndLibraries} />
+        <SkillCategory title="Systems & Databases" skills={systemsAndDatabases} />
+        <SkillCategory title="Testing Frameworks" skills={testingFrameworks} />
+        <SkillCategory title="Graphic Design" skills={graphicDesign} />
+      </div>
     </div>
   );
 };
