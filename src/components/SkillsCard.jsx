05f97c328d66c7ec1e56ff094b3b74a60310dafd
Ana Franco <anamspe.fr@gmail.com>
2024-04-15 17:52:44 -0700

    Install React Tooltip and add anchor to section

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,8 +1,9 @@
 import icons from "../assets/icons";
+import { Tooltip } from "react-tooltip";
 
 const SkillCategory = ({ title, skills }) => {
   return (
-    <div id="skills">
+    <div>
       <h2 className="ms-4 my-4 font-bold underline underline-offset-2">
         {title}
       </h2>
@@ -11,8 +12,12 @@ const SkillCategory = ({ title, skills }) => {
           <li
             key={index}
             className="inline-block transition-all duration-500 ease-in-out hover:opacity-70"
+            data-tooltip-id="skill-tt"
+            data-tooltip-content={skill.alt}            
+            data-tooltip-delay-show={300}
             title={skill.alt}
           >
+            <Tooltip id="skill-tt" place="bottom-start"/>
             <img
               className="size-12 bg-cadet bg-opacity-50 mr-2 p-1 rounded "
               src={skill.src}
@@ -62,8 +67,8 @@ const Skills = () => {
   ];
 
   return (
-    <div className="flex flex-col max-w-5xl mt-2 mx-auto p-10">
-      <h2 className="text-4xl font-arima font-bold text-cherry p-0 mb-4">
+    <div className="flex flex-col max-w-5xl mt-2 mx-auto p-10" id="skills">
+      <h2 className="text-4xl mt-12 font-arima font-bold text-cherry p-0 mb-4">
         Skills
       </h2>
       <SkillCategory
