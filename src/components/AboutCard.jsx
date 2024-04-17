858599dc60c7234a329e9797e6f6babf20f2a22c
Ana Franco <anamspe.fr@gmail.com>
2024-04-16 21:57:24 -0700

    Update about me content and UI

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,38 +1,77 @@
 const About = () => {
   return (
-    <div className="flex flex-col max-w-5xl mt-2 mx-auto p-10" id="about">
-      <h2 className="text-4xl font-arima font-bold text-cherry p-0 mb-4">
-        About me
-      </h2>
-      <span className="mx-6">▪ Bio</span>
-      <div>
-        🔹 About Me: With a background in Communications and a knack for
-        storytelling honed during my time as a video editor, I've recently
-        embarked on an exciting journey into the world of web development. Armed
-        with diplomas in television and video production from BCIT and
-        full-stack web development from Lighthouse Labs, I'm passionate about
-        leveraging technology to create innovative solutions and engaging user
-        experiences.
-      </div>{" "}
-      <div>
-        {" "}
-        🔹 Journey & Identity: Born in vibrant Brazil and now proudly calling
-        Canada home, my journey as an immigrant has infused my work with diverse
-        perspectives and a deep appreciation for cultural exchange. Achieving
-        Canadian citizenship last year was a milestone that solidified my
-        commitment to contributing to and thriving in my adopted community.
-      </div>{" "}
-      <div>
-        {" "}
-        🔹 Interests & Hobbies: Beyond coding, I'm an avid enthusiast of video
-        games, music, and cinema. Exploring the intersections of art and
-        technology fuels my creativity, whether I'm crafting code or immersing
-        myself in the latest indie film or gallery exhibit. When not in front of
-        a screen, you'll likely find me planning my next adventure, whether it's
-        a road trip to explore Canada's natural wonders or a journey to discover
-        new culinary delights around the globe.
+    <section className="flex flex-col max-w-5xl mt-2 mx-auto p-10 " id="about">
+      <h2 className="text-4xl mt-12 font-arima font-bold text-cherry ">About me</h2>
+      <span className="mx-6 font-wellfleet">▪ A little bit about myself</span>
+      <div className="max-w-3xl mt-10 mx-auto">
+        <p className="text-center">
+          Hi! I'm Ana Franco. <span className="font-emoji">👋</span>
+        </p>
+        <p className="my-4">
+          I'm a Junior Full Stack Developer, with a background in Communications
+          and Video Editing. I have a big aptitude for storytelling, which was
+          largely practiced during my time as a video editor, but I've recently
+          decided to dive deep into the world of web development. My first
+          connection with coding was when as a teenager, I kept a blog and
+          decided it was time to learn at least the basics so I had my own input
+          in how my blog looked. Since then, I've exercised my creativity in
+          many different ways, but coding was always something I wanted to get
+          back to. Now, armed with a full-stack web development diploma from
+          Lighthouse Labs, I'm eager to expand my skills as a developer, and
+          also make use of skills from my past works to create innovative
+          products and engaging user experiences.
+        </p>
+        <div>
+          <p className="text-xl text-bold font-wellfleet">Education</p>
+          <p>
+            <span className="font-emoji">🎓</span> Full Stack Web Development
+            Diploma - Lighthouse Labs
+          </p>
+          <p>
+            <span className="font-emoji">🎓</span> Television and Video
+            Production Diploma - British Columbia Institute of Technology (BCIT)
+          </p>
+          <p>
+            <span className="font-emoji">🎓</span> Bachelor's Degree in
+            Communications - Universidade Federal do Maranhão (Brazil)
+          </p>
+        </div>
       </div>
-    </div>
+
+      <div className="flex mt-12 gap-6 justify-center">
+        <div className="p-6 bg-tyrian rounded-lg max-w-xs">
+          <h2 className="text-xl text-bold font-wellfleet text-center">
+            Journey & Identity
+          </h2>
+          <p className="text-sm my-4">
+            I was born in Brazil but now I proudly call Canada home. My journey
+            as an immigrant has had a big influence on my work, awarding me
+            diverse perspectives and an appreciation for cultural exchange.
+          </p>
+          <p className="text-sm my-4">
+            I'm genuinely excited about connecting with people, exchanging ideas
+            and making meaningful contributions to my work and community. I like
+            bringing a cheerful and enthusiastic approach to my work, because I
+            believe that a positive mindset plays a big role in our confidence
+            and competence.
+          </p>
+        </div>
+
+        <div className="p-6 bg-tyrian rounded-lg max-w-xs">
+          <h2 className="text-xl text-bold font-wellfleet text-center">
+            Interests & Hobbies
+          </h2>
+          <p className="text-sm my-4">
+            Beyond coding, I'm a video games, music, and cinema lover. Exploring
+            art and technology and finding common points between them fuel my
+            creativity, whether I'm creating smart code or editing my personal
+            video content. When not in front of a screen, I also enjoy road
+            trips - my favourite way of exploring Canada's natural wonders - and
+            baking the most delicious breads and desserts.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
