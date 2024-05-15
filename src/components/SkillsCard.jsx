e182055956946121284ef39689a8d11fa9969179
Ana Franco <anamspe.fr@gmail.com>
2024-05-14 19:39:43 -0700

    Update Skills section for mobile

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -3,13 +3,15 @@ import { Tooltip } from "react-tooltip";
 
 const SkillCategory = ({ title, skills }) => {
   return (
-    <div className="flex flex-wrap sm:block max-w-48 justify-center">
-      <h2 className="mb-4 font-bold min-w-60">{title}</h2>
-      <div className="flex flex-wrap justify-center">
+    <div className="flex flex-wrap sm:block sm:max-w-48">
+      <h2 className="mb-4 ml-20 sm:ml-0 font-bold min-w-60">
+        {title}
+      </h2>
+      <div className="flex flex-col sm:flex-row sm:flex-wrap sm:gap-0 sm:justify-center">
         {skills.map((skill, index) => (
           <div
             key={index}
-            className="transition-all duration-500 ease-in-out hover:opacity-70"
+            className="inline-flex transition-all duration-500 ease-in-out hover:opacity-70"
             data-tooltip-id="skill-tt"
             data-tooltip-content={skill.alt}
             data-tooltip-delay-show={300}
@@ -20,6 +22,7 @@ const SkillCategory = ({ title, skills }) => {
               src={skill.src}
               alt={skill.alt}
             />
+            <p className="sm:hidden place-content-center">{skill.alt}</p>
           </div>
         ))}
       </div>
@@ -69,13 +72,13 @@ const Skills = () => {
       className="max-w-7xl mx-auto my-5 p-10 content-center scroll-mt-20 section-min-height"
       id="skills"
     >
-      <h2 className="text-4xl mt-12 pl-32 font-arima font-bold text-cherry">
+      <h2 className="text-4xl sm:pl-32 font-arima font-bold text-cherry">
         Skills
       </h2>
-      <span className="mx-6 pl-32 font-wellfleet">
+      <span className="mx-6 sm:pl-32 font-wellfleet">
         ▪ My technical abilities
       </span>
-      <div className="flex flex-col sm:flex-row p-10 my-16 place-content-around bg-tyrian drop-shadow-sm shadow-paynes rounded-3xl">
+      <div className="flex flex-col sm:flex-row p-4 sm:p-10 my-4 sm:my-16 items-center sm:items-start gap-10 sm:gap-0 place-content-around bg-tyrian drop-shadow-sm shadow-paynes rounded-3xl">
         <SkillCategory
           title="Programming Languages"
           skills={programmingLanguages}
